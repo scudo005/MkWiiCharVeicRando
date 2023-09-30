8c6a533e5bb8566f83ada3f76bfb180c8f470c8c
DocSwitch <whiteshield.tg@protonmail.com>
2023-09-30 21:28:54 +0200

    translating everything to english in main branch

--- MkWiiCharVeicRando.cs
+++ MkWiiCharVeicRando.cs
@@ -7,11 +7,11 @@ internal class MKWiiCharVeicRando
         int currentVeichleID = 0;
         var rng = new Random();
     InsertNoPlayers:
-        Console.WriteLine("Inserire il numero di giocatori: ");
+        Console.WriteLine("Insert the number of players: ");
         int numberOfPlayers = int.Parse(Console.ReadLine()!);
         if (numberOfPlayers > 12 || numberOfPlayers <= 0)
         {
-            Console.WriteLine("Hai inserito un numero impossibile di giocatori. ");
+            Console.WriteLine("You have inserted an impossible amount of players. ");
             goto InsertNoPlayers; //hate the game not the player
         }
 
@@ -20,58 +20,57 @@ internal class MKWiiCharVeicRando
             currentCharacterID = rng.Next(0, 25);
             currentVeichleID = rng.Next(0, 11);
             var sb = new System.Text.StringBuilder();
-            int j = i+1;
-            string startText = "Risultato giocatore " + j + ": \nPersonaggio: ";
-            sb.Append(startText);
+            int j = i+1; 
+            sb.Append("Player no." + j + ": Character: ");
                 switch (currentCharacterID){
                     case 0:
-                        sb.Append("Baby Mario\nVeicolo: ");
+                        sb.Append("Baby Mario\nVeichle: ");
                         break;
                     case 1:
-                        sb.Append("Baby Luigi\nVeicolo: ");
+                        sb.Append("Baby Luigi\nVeichle: ");
                         break;
                     case 2:
-                        sb.Append("Baby Peach\nVeicolo: ");
+                        sb.Append("Baby Peach\nVeichle: ");
                         break;
                     case 3:
-                        sb.Append("Baby Daisy\nVeicolo: ");
+                        sb.Append("Baby Daisy\nVeichle: ");
                         break;
                     case 4:
-                        sb.Append("Toad\nVeicolo: ");
+                        sb.Append("Toad\nVeichle: ");
                         break;
                     case 5:
-                        sb.Append("Toadette\nVeicolo: ");
+                        sb.Append("Toadette\nVeichle: ");
                         break;
                     case 6:
-                        sb.Append("Koopa\nVeicolo: ");
+                        sb.Append("Koopa Troopa\nVeichle: ");
                         break;
                     case 7:
-                        sb.Append("Tartosso\nVeicolo: ");
+                        sb.Append("Dry Bones\nVeichle: ");
                         break;
                     //lightweights range from ID 0 to ID 7
                     case 8:
-                        sb.Append("Mario\nVeicolo: ");
+                        sb.Append("Mario\nVeichle: ");
                         break;
                     case 9:
-                        sb.Append("Luigi\nVeicolo: ");
+                        sb.Append("Luigi\nVeichle: ");
                         break;
                     case 10:
-                        sb.Append("Daisy\nVeicolo: ");
+                        sb.Append("Daisy\nVeichle: ");
                         break;
                     case 11:
-                        sb.Append("Peach\nVeicolo: ");
+                        sb.Append("Peach\nVeichle: ");
                         break;
                     case 12:
-                        sb.Append("Yoshi\nVeicolo: ");
+                        sb.Append("Yoshi\nVeichle: ");
                         break;
                     case 13:
-                        sb.Append("Strutzi\nVeicolo: ");
+                        sb.Append("Birdo\nVeichle: ");
                         break;
                     case 14:
-                        sb.Append("Diddy Kong\nVeicolo: ");
+                        sb.Append("Diddy Kong\nVeichle: ");
                         break;
                     case 15:
-                        sb.Append("Bowser Junior\nVeicolo: ");
+                        sb.Append("Bowser Junior\nVeichle: ");
                         break;
                     //middleweights range from ID 8 to ID 15
                     //Mii Outfit A & B are special cases; they get their own ID.
@@ -80,80 +79,80 @@ internal class MKWiiCharVeicRando
                     //Maybe if the program is actually used I might implement such a feature, but for now I'll assume
                     //That Miis are all middleweight. Feel free to open an issue if you care.
                     case 16:
-                        sb.Append("Wario\nVeicolo: ");
+                        sb.Append("Wario\nVeichle: ");
                         break;
                     case 17:
-                        sb.Append("Waluigi\nVeicolo: ");
+                        sb.Append("Waluigi\nVeichle: ");
                         break;
                     case 18:
-                        sb.Append("Donkey Kong\nVeicolo: ");
+                        sb.Append("Donkey Kong\nVeichle: ");
                         break;
                     case 19:
-                        sb.Append("Bowser\nVeicolo: ");
+                        sb.Append("Bowser\nVeichle: ");
                         break;
                     case 20:
-                        sb.Append("Re Boo\nVeicolo: ");
+                        sb.Append("King Boo\nVeichle: ");
                         break;
                     case 21:
-                        sb.Append("Rosalinda\nVeicolo: ");
+                        sb.Append("Rosalina\nVeichle: ");
                         break;
                     case 22:
-                        sb.Append("Funky Kong\nVeicolo: ");
+                        sb.Append("Funky Kong\nVeichle: ");
                         break;
                     case 23:
-                        sb.Append("Skelobowser\nVeicolo: ");
+                        sb.Append("Dry Bowser\nVeichle: ");
                         break;
                     case 24:
-                        sb.Append("Mii Outfit A\nVeicolo: ");
+                        sb.Append("Mii Outfit A\nVeichle: ");
                         break;
                     case 25:
-                        sb.Append("Mii Outfit B\nVeicolo: ");
+                        sb.Append("Mii Outfit B\nVeichle: ");
                         break;
                     default:
-                        Console.WriteLine("Opzione non riconosciuta: "+currentCharacterID);
+                        Console.WriteLine("Unrecognized option: "+currentCharacterID);
                         break;
                 }
             if (currentCharacterID >= 0 && currentCharacterID <= 7)
             { //lightweight
                 switch (currentVeichleID){
                     case 0:
-                        sb.Append("Kart standard P\n");
+                        sb.Append("Standard kart S\n");
                         break;
                     case 1:
-                        sb.Append("Culla a reazione\n");
+                        sb.Append("Booster Seat\n");
                         break;
                     case 2:
-                        sb.Append("Pistoni bollenti\n");
+                        sb.Append("Mini Beast\n");
                         break;
                     case 3:
-                        sb.Append("Smack-mobile\n");
+                        sb.Append("Cheep Charger\n");
                         break;
                     case 4:
-                        sb.Append("Minirally\n");
+                        sb.Append("Tiny Titan\n");
                         break;
                     case 5:
                         sb.Append("Blue Falcon\n");
                         break;
                     case 6:
-                        sb.Append("Moto standard P\n");
+                        sb.Append("Standard bike S\n");
                         break;
                     case 7:
-                        sb.Append("Moto pallottolo\n");
+                        sb.Append("Bullet Bike\n");
                         break;
                     case 8:
-                        sb.Append("Moto Bit\n");
+                        sb.Append("Bit Bike\n");
                         break;
                     case 9:
-                        sb.Append("Quaquaraqua\n");
+                        sb.Append("Quacker\n");
                         break;
                     case 10:
                         sb.Append("Kamekruiser\n");
                         break;
                     case 11:
-                        sb.Append("Bolla jet\n");
+                        sb.Append("Jet Bubble\n");
                         break;
                     default:
-                        Console.WriteLine("Opzione non riconosciuta: "+currentVeichleID);
+                        Console.WriteLine("Unrecognized option: "+currentVeichleID);
                         break;                        
                 }
             }
@@ -161,43 +160,43 @@ internal class MKWiiCharVeicRando
             { //middleweight
                     switch (currentVeichleID){
                     case 0:
-                        sb.Append("Kart standard M\n");
+                        sb.Append("Standard kart M\n");
                         break;
                     case 1:
-                        sb.Append("Nostalgia 1\n");
+                        sb.Append("Classic Dragster");
                         break;
                     case 2:
-                        sb.Append("Ali di squalo\n");
+                        sb.Append("Wild Wing\n");
                         break;
                     case 3:
-                        sb.Append("Super calamako\n");
+                        sb.Append("Super blooper\n");
                         break;
                     case 4:
-                        sb.Append("Bolide retrò\n");
+                        sb.Append("Daytripper\n");
                         break;
                     case 5:
-                        sb.Append("Glory\n");
+                        sb.Append("B Dasher Mk 2\n");
                         break;
                     case 6:
-                        sb.Append("Moto standard M\n");
+                        sb.Append("Standard bike M\n");
                         break;
                     case 7:
-                        sb.Append("Moto Mach\n");
+                        sb.Append("Mach Bike\n");
                         break;
                     case 8:
-                        sb.Append("Scooter filante\n");
+                        sb.Append("Sugarscoot\n");
                         break;
                     case 9:
-                        sb.Append("Jalapeno\n");
+                        sb.Append("Zip Zip\n");
                         break;
                     case 10:
-                        sb.Append("Nitrocicletta\n");
+                        sb.Append("Sneakster\n");
                         break;
                     case 11:
-                        sb.Append("Delfinator\n");
+                        sb.Append("Dolphin Dasher\n");
                         break;
                     default:
-                        Console.WriteLine("Opzione non riconosciuta: "+currentVeichleID);
+                        Console.WriteLine("Unrecognized option: "+currentVeichleID);
                         break;                        
                 }
             }
@@ -205,50 +204,50 @@ internal class MKWiiCharVeicRando
             {  //heavyweight
                 switch (currentVeichleID){
                     case 0:
-                        sb.Append("Kart standard G\n");
+                        sb.Append("Standard kart L\n");
                         break;
                     case 1:
-                        sb.Append("Dune buggy\n");
+                        sb.Append("Offroader\n");
                         break;
                     case 2:
-                        sb.Append("Fiamma volante\n");
+                        sb.Append("Flame Flyer\n");
                         break;
                     case 3:
-                        sb.Append("Piranha turbo\n");
+                        sb.Append("Piranha Prowler\n");
                         break;
                     case 4:
-                        sb.Append("Protoracer\n");
+                        sb.Append("Jetsetter\n");
                         break;
                     case 5:
-                        sb.Append("Flash GT\n");
+                        sb.Append("Honeycoupe\n");
                         break;
                     case 6:
-                        sb.Append("Moto standard G\n");
+                        sb.Append("Standard bike L\n");
                         break;
                     case 7:
-                        sb.Append("Moto Bowser\n");
+                        sb.Append("Flame Runner\n");
                         break;
                     case 8:
-                        sb.Append("Moto di Wario\n");
+                        sb.Append("Wario Bike\n");
                         break;
                     case 9:
-                        sb.Append("Moto Stella\n");
+                        sb.Append("Shooting Star\n");
                         break;
                     case 10:
-                        sb.Append("Torpedo sprint\n");
+                        sb.Append("Torpedo\n");
                         break;
                     case 11:
-                        sb.Append("Motorazzo\n");
+                        sb.Append("Phantom\n");
                         break;
                     default:
-                        Console.WriteLine("Opzione non riconosciuta: "+currentVeichleID);
+                        Console.WriteLine("Unrecognized option: "+currentVeichleID);
                         break;                        
                 }
             }
 
             Console.WriteLine(sb.ToString());
         }
-        Console.WriteLine("Premere un tasto qualsiasi per uscire.");
+        Console.WriteLine("Press any key to exit the program.");
         Console.ReadKey();
     }
     
